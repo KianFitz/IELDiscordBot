6753623910b21d744072db51cd30d11ddc9e551d
KianFitz <kian.fitzpatrick2000@gmail.com>
2021-04-23 20:31:22 +0100

    Try twitter accounts again

--- WebAppAPI/Controllers/ApplicationController.cs
+++ WebAppAPI/Controllers/ApplicationController.cs
@@ -34,7 +34,7 @@ namespace WebAppAPI.Controllers
                 objList.Add(@$"https://webapp.imperialesportsleague.co.uk/user/{signup.user.id}");
                 objList.Add(signup.user.referral);
                 objList.Add(signup.feedback);
-                //objList.AddRange(signup.socialAccounts.Where(x => x.active && x.type == "twitter").Select(x => x.platform_name).Take(17));
+                objList.AddRange(signup.socialAccounts.Where(x => x.active && x.type == "twitter").Select(x => x.platform_name).Take(17));
 
                 int rowNumber = SpreadsheetService.Instance().GetNextAvailableRow();
                 await SpreadsheetService.Instance().MakeRequest($"Player Data!A:H{rowNumber}", objList).ConfigureAwait(false);
