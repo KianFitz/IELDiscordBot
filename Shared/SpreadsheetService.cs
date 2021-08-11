dd876a6bd9a395a26bcb23b1f624cdc762d329d6
Kian Fitzpatrick <kian.fitzpatrick2000@gmail.com>
2021-08-11 11:03:45 +0100

    Move to S10 Spreadsheet

--- Shared/SpreadsheetService.cs
+++ Shared/SpreadsheetService.cs
@@ -15,7 +15,7 @@ namespace Shared
         ServiceAccountCredential _sheetsCredential;
         string[] Scopes = { SheetsService.Scope.Spreadsheets };
         const string ApplicationName = "IEL Discord Bot .NET Application";
-        const string SpreadsheetID = "1Yf38nVz_WD3VBf74LTjnt75NNLvAp54SySrvt2PRf3I";
+        const string SpreadsheetID = "1QyixxLA2jl1p_K1TlL5mp9Fsa8xsVa402iwSXo4rvMQ";
         const string ServiceAccountEmail = "ieldiscordbot@inspired-rock-284217.iam.gserviceaccount.com";
         private SheetsService service;
         private IList<IList<object>> _latestValues = null;
